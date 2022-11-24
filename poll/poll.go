e2e0ea10a9ca6869093d01fbd02dfaacc2d6bf1a
Antoine Eiche <lewo@abesis.fr>
2022-11-24 22:04:42 +0100

    Add a state file to record the last operation
    This is useful when we switch from testing to main: we want to run
    switch while on the testing branch we were running the nixos-rebuild
    test operation.

--- poll/poll.go
+++ poll/poll.go
@@ -3,12 +3,15 @@ package poll
 import (
 	"github.com/go-co-op/gocron"
 	"github.com/sirupsen/logrus"
+	"os"
 	"fmt"
 	"time"
 	"github.com/nlewo/comin/types"
 	"github.com/nlewo/comin/nix"
 	cominGit "github.com/nlewo/comin/git"
 	"github.com/go-git/go-git/v5"
+	"io/ioutil"
+	"encoding/json"
 )
 
 func Poller(hostname string, stateDir string, dryRun bool, repositories []string) error {
@@ -22,7 +25,11 @@ func Poller(hostname string, stateDir string, dryRun bool, repositories []string
 	logrus.Infof("Polling every %d seconds to deploy the machine '%s'", period, hostname)
 	job, _ := s.Every(period).Second().Tag("poll").Do(
 		func () error {
-			return poll(repository, config)
+			err := poll(repository, config)
+			if err != nil {
+				logrus.Error(err)
+			}
+			return err
 		})
 	job.SingletonMode()
 	s.StartBlocking()
@@ -33,6 +40,7 @@ func makeConfig(stateDir string, hostname string, dryRun bool, repositories []st
 	return types.Config{
 		Hostname: hostname,
 		StateDir: stateDir,
+		StateFile: fmt.Sprintf("%s/state.json", stateDir),
 		GitConfig: types.GitConfig{
 			Path: fmt.Sprintf("%s/repository", stateDir),
 			Remote: types.Remote{
@@ -51,21 +59,45 @@ func poll(repository *git.Repository, config types.Config) error {
 
 	hasNewCommits, isTesting, err := cominGit.RepositoryUpdate(repository, config.GitConfig)
 	if err != nil {
-		logrus.Error(err)
 		return err
 	}
-	if !hasNewCommits {
-		return nil
-	}
-
 	operation := "switch"
 	if isTesting {
 		operation = "test"
 	}
+
+	var state types.State
+        if _, err := os.Stat(config.StateFile); err == nil {
+		logrus.Debugf("Loading state file")
+		content, err := ioutil.ReadFile(config.StateFile)
+		if err != nil {
+			return err
+		}
+		err = json.Unmarshal(content, &state)
+		if err != nil {
+			return err
+		}
+		logrus.Debugf("State is %#v", state)
+	}
+
+	if !hasNewCommits && state.LastOperation == operation {
+		return nil
+	}
+
 	err = nix.Deploy(config, operation)
 	if err != nil {
-		logrus.Error(err)
 		return err
 	}
+	state.LastOperation = operation
+
+	res, err := json.MarshalIndent(state, "", "\t")
+	if err != nil {
+		return err
+	}
+	err = ioutil.WriteFile(config.StateFile, []byte(res), 0644)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
--- types/types.go
+++ types/types.go
@@ -1,8 +1,15 @@
 package types
 
+type State struct {
+	// LastOperation is the last nixos-rebuild operation
+	// (basically, test or switch)
+	LastOperation string
+}
+
 type Config struct {
 	Hostname string
 	StateDir string
+	StateFile string
 	GitConfig GitConfig
 	DryRun bool
 }
