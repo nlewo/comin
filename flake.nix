224d263e53b915a49c5e53043f81b3e6085804d7
Antoine Eiche <lewo@abesis.fr>
2022-12-31 10:16:26 +0100

    Add flake.nix

---
+++ flake.nix
@@ -0,0 +1,14 @@
+{
+  description = "Comin";
+
+  outputs = { self, nixpkgs }: {
+    devShell.x86_64-linux =
+      let
+        pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      in pkgs.mkShell {
+        buildInputs = [
+          pkgs.go pkgs.godef
+        ];
+      };
+  };
+}
