5319cb10bd08c4a9facd9f6f415ee3d37dc27ab1
Antoine Eiche <lewo@abesis.fr>
2023-01-02 19:13:17 +0100

    flake: add NixOS module

--- flake.nix
+++ flake.nix
@@ -29,8 +29,63 @@
         vendorSha256 = "sha256-7P//MF0ZRDihabSgbxhqxilDrwhZPjMRGblZXBdNT2E=";
       };
     };
+
     packages.x86_64-linux.comin = pkgs.comin;
     defaultPackage.x86_64-linux = pkgs.comin;
+
+    nixosModules.comin = { config, pkgs, lib, ... }: let
+      cfg = config.services.comin;
+    in {
+      options = {
+        services.comin = {
+          enable = lib.mkOption {
+            type = lib.types.bool;
+            default = false;
+            description = ''
+              Whether to run the comin service.
+            '';
+          };
+          repository = lib.mkOption {
+            type = lib.types.str;
+            description = ''
+              The repository to poll.
+            '';
+          };
+          authFile = lib.mkOption {
+            type = lib.types.nullOr lib.types.str;
+            default = null;
+            description = ''
+              The path of the auth file.
+            '';
+          };
+          debug = lib.mkOption {
+            type = lib.types.bool;
+            default = false;
+            description = ''
+              Whether to run comin in debug mode. Be careful, secrets are shown!.
+            '';
+          };
+        };
+      };
+      config = {
+        nixpkgs.overlays = [ self.overlay ];
+        systemd.services.comin = {
+          wantedBy = [ "multi-user.target" ];
+          path = [ pkgs.nix pkgs.git ];
+          serviceConfig = {
+            ExecStart =
+              "${pkgs.comin}/bin/comin "
+              + (lib.optionalString cfg.debug "--debug ")
+              + " poll "
+              + "'${cfg.repository}' "
+              + "--period 10 "
+              + (lib.optionalString (cfg.authFile != null) "--auths-file ${cfg.authFile}");
+              Restart = "always";
+          };
+        };
+      };
+    };
+
     devShell.x86_64-linux = let
       pkgs = nixpkgs.legacyPackages.x86_64-linux;
     in pkgs.mkShell {
