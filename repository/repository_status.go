1cc2c8f8481d2bf55a87328d7cb4cf505f1b08a9
Antoine Eiche <lewo@abesis.fr>
2023-08-29 21:43:26 +0000

    Add json annotations

--- repository/repository_status.go
+++ repository/repository_status.go
@@ -7,49 +7,41 @@ import (
 	"time"
 )
 
-type Branch struct {
-	Name string
-	CommitId string
-	Error error
-}
-
 type MainBranch struct {
-	Name string
-	CommitId string
-	ErrorMsg string
-
-	OnTopOf string
+	Name string `json:"name,omitempty"`
+	CommitId string `json:"commit_id,omitempty"`
+	ErrorMsg string `json:"error_msg,omitempty"`
+	OnTopOf string `json:"on_top_of,omitempty"`
 }
 
 type TestingBranch struct {
-	Name string
-	CommitId string
-	ErrorMsg string
-
-	OnTopOf string
+	Name string `json:"name,omitempty"`
+	CommitId string `json:"commit_id,omitempty"`
+	ErrorMsg string `json:"error_msg,omitempty"`
+	OnTopOf string `json:"on_top_of,omitempty"`
 }
 
 type Remote struct {
-	Name string
-	Url string
-	FetchErrorMsg string
-	Main *MainBranch
-	Testing *TestingBranch
-	FetchedAt time.Time
-	Fetched bool
+	Name string `json:"name,omitempty"`
+	Url string `json:"url,omitempty"`
+	FetchErrorMsg string `json:"fetch_error_msg,omitempty"`
+	Main *MainBranch `json:"main,omitempty"`
+	Testing *TestingBranch `json:"testing,omitempty"`
+	FetchedAt time.Time `json:"fetched_at,omitempty"`
+	Fetched bool `json:"fetched,omitempty"`
 }
 
 type RepositoryStatus struct {
 	// This is the deployed Main commit ID. It is used to ensure
 	// fast forward
-	SelectedCommitId string
-	SelectedRemoteName string
-	SelectedBranchName string
-	SelectedBranchIsTesting bool
-	MainCommitId string
-	MainRemoteName string
-	MainBranchName string
-	Remotes []*Remote
+	SelectedCommitId string `json:"selected_commit_id"`
+	SelectedRemoteName string `json:"selected_remote_name"`
+	SelectedBranchName string  `json:"selected_branch_name"`
+	SelectedBranchIsTesting bool `json:"selected_branch_is_testing"`
+	MainCommitId string `json:"main_commit_id"`
+	MainRemoteName string `json:"main_remote_name"`
+	MainBranchName string `json:"main_branch_name"`
+	Remotes []*Remote `json:"remotes"`
 }
 
 func NewRepositoryStatus(config types.GitConfig, repositoryStatus RepositoryStatus) RepositoryStatus {
