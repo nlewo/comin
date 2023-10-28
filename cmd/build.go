f812390c731db495d0ef73dc359cd7c91283cadf
Antoine Eiche <lewo@abesis.fr>
2023-10-28 16:43:04 +0200

    Command build builds all NixOS configurations by default

--- cmd/build.go
+++ cmd/build.go
@@ -4,23 +4,31 @@ import (
 	"github.com/nlewo/comin/nix"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
-	"os"
 )
 
 var buildCmd = &cobra.Command{
 	Use:   "build",
-	Short: "Build a machine from a local repository",
+	Short: "Build a machine configuration",
 	Args:  cobra.MinimumNArgs(0),
 	Run: func(cmd *cobra.Command, args []string) {
-		nix.Build(".", hostname)
+		hosts := make([]string, 1)
+		if hostname != "" {
+			hosts[0] = hostname
+		} else {
+			hosts, _ = nix.List(flakeUrl)
+		}
+		for _, host := range hosts {
+			logrus.Infof("Building the NixOS configuration of machine '%s'", host)
+			_, err := nix.Build(flakeUrl, host)
+			if err != nil {
+				logrus.Errorf("Failed to build the configuration '%s': '%s'", host, err)
+			}
+		}
 	},
 }
 
 func init() {
-	hostnameDefault, err := os.Hostname()
-	if err != nil {
-		logrus.Error(err)
-	}
-	buildCmd.Flags().StringVarP(&hostname, "hostname", "", hostnameDefault, "the name of the configuration to deploy")
+	buildCmd.Flags().StringVarP(&hostname, "hostname", "", "", "the name of the configuration to build")
+	buildCmd.Flags().StringVarP(&flakeUrl, "flake-url", "", ".", "the URL of the flake")
 	rootCmd.AddCommand(buildCmd)
 }
