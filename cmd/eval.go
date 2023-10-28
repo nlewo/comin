2f7e7e137aa5047c1e261b16549a0a4fe14f4e74
Antoine Eiche <lewo@abesis.fr>
2023-10-28 15:38:58 +0200

    Command eval evaluates all NixOS configurations

--- cmd/eval.go
+++ cmd/eval.go
@@ -4,7 +4,6 @@ import (
 	"github.com/nlewo/comin/nix"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
-	"os"
 )
 
 var evalCmd = &cobra.Command{
@@ -12,18 +11,24 @@ var evalCmd = &cobra.Command{
 	Short: "Eval a machine from a local repository",
 	Args:  cobra.MinimumNArgs(0),
 	Run: func(cmd *cobra.Command, args []string) {
-		_, _, err := nix.ShowDerivation(".", hostname)
-		if err != nil {
-			logrus.Errorf("Failed to eval the configuration '%s': '%s'", hostname, err)
+		hosts := make([]string, 1)
+		if hostname != "" {
+			hosts[0] = hostname
+		} else {
+			hosts, _ = nix.List(flakeUrl)
+		}
+		for _, host := range hosts {
+			logrus.Infof("Evaluating the NixOS configuration of machine '%s'", host)
+			_, _, err := nix.ShowDerivation(flakeUrl, host)
+			if err != nil {
+				logrus.Errorf("Failed to eval the configuration '%s': '%s'", host, err)
+			}
 		}
 	},
 }
 
 func init() {
-	hostnameDefault, err := os.Hostname()
-	if err != nil {
-		logrus.Error(err)
-	}
-	evalCmd.Flags().StringVarP(&hostname, "hostname", "", hostnameDefault, "the name of the configuration to eval")
+	evalCmd.Flags().StringVarP(&hostname, "hostname", "", "", "the name of the configuration to eval")
+	evalCmd.Flags().StringVarP(&flakeUrl, "flake-url", "", ".", "the URL of the flake")
 	rootCmd.AddCommand(evalCmd)
 }
