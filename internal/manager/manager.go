337d2a20c46abaeeb1a50b56633bece6ed802dee
rw <rw@jjk.is>
2024-07-09 23:53:44 -0700

    rm

--- internal/manager/manager.go
+++ internal/manager/manager.go
@@ -12,7 +12,6 @@ import (
 	"github.com/nlewo/comin/internal/store"
 	"github.com/nlewo/comin/internal/utils"
 	"github.com/sirupsen/logrus"
-	"golang.org/x/crypto/nacl/auth"
 )
 
 type State struct {
@@ -97,13 +96,13 @@ func (m Manager) Fetch(remotes []string) {
 	// todo check if remote is finished building 
 	// if it is, then fetch
 	// if not, then wait
-	deploymentID := panic("not implemented")
-	authToken := panic("not implemented")
-	resp := http.Get(fmt.Sprintf("http://localhost:8080/api/v1/deployments/%s", deploymentID), authToken)
-	if resp.Status != "success" {
-		// wait
-		return
-	}
+	// deploymentID := panic("not implemented")
+	// authToken := panic("not implemented")
+	// resp := http.Get(fmt.Sprintf("http://localhost:8080/api/v1/deployments/%s", deploymentID), authToken)
+	// if resp.Status != "success" {
+	// 	// wait
+	// 	return
+	// }
 
 	m.triggerRepository <- remotes
 }
