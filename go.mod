116a559de2d895a4dc9de179f876de8f83e6a030
xinyangli <lixinyang411@gmail.com>
2024-12-20 19:20:45 +0800

    Add Garnix executor
    - add `executor` option in config and nixos module
    - evaluate and fetch build result from garnix with minimal memory
    footprint
    - add test and doc for executors

--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/dustin/go-humanize v1.0.1
 	github.com/go-co-op/gocron/v2 v2.11.0
 	github.com/go-git/go-git/v5 v5.11.0
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da
 	github.com/google/uuid v1.6.0
 	github.com/prometheus/client_golang v1.19.0
 	github.com/sirupsen/logrus v1.9.3
@@ -27,7 +28,6 @@ require (
 	github.com/emirpasic/gods v1.18.1 // indirect
 	github.com/go-git/gcfg v1.5.1-0.20230307220236-3a3c6141e376 // indirect
 	github.com/go-git/go-billy/v5 v5.5.0 // indirect
-	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/jbenet/go-context v0.0.0-20150711004518-d14ea06fba99 // indirect
 	github.com/jonboulle/clockwork v0.4.0 // indirect
--- nix/module-options.nix
+++ nix/module-options.nix
@@ -1,192 +1,246 @@
-{ config, pkgs, lib, ... }: {
-  options = with lib; with types; {
-    services.comin = {
-      enable = mkOption {
-        type = types.bool;
-        default = false;
-        description = ''
-          Whether to run the comin service.
-        '';
-      };
-      package = lib.mkPackageOption pkgs "comin" { nullable = true; } // {
-        defaultText = "pkgs.comin or comin.packages.\${system}.default or null";
-      };
-      hostname = mkOption {
-        type = str;
-        default = config.networking.hostName;
-        description = ''
-          The name of the configuration to evaluate and deploy. 
-          This value is used by comin to evaluate the flake output
-          nixosConfigurations."<hostname>" or darwinConfigurations."<hostname>".
-          Defaults to networking.hostName - you MUST set either this option
-          or networking.hostName in your configuration.
-        '';
-      };
-      flakeSubdirectory = mkOption {
-        type = str;
-        default = ".";
-        description = ''
-          Subdirectory in the repository, containing flake.nix.
-        '';
-      };
-      exporter = mkOption {
-        description = "Options for the Prometheus exporter.";
-        default = {};
-        type = submodule {
-          options = {
-            listen_address = mkOption {
-              type = str;
-              description = ''
-                Address to listen on for the Prometheus exporter. Empty string will listen on all interfaces.
-              '';
-              default = "";
-            };
-            port = mkOption {
-              type = int;
-              description = ''
-                Port to listen on for the Prometheus exporter.
-              '';
-              default = 4243;
-            };
-            openFirewall = mkOption {
-              type = types.bool;
-              default = false;
-              description = ''
-                Open port in firewall for incoming connections to the Prometheus exporter.
-              '';
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+{
+  options =
+    with lib;
+    with types;
+    {
+      services.comin = {
+        enable = mkOption {
+          type = types.bool;
+          default = false;
+          description = ''
+            Whether to run the comin service.
+          '';
+        };
+        package = lib.mkPackageOption pkgs "comin" { nullable = true; } // {
+          defaultText = "pkgs.comin or comin.packages.\${system}.default or null";
+        };
+        hostname = mkOption {
+          type = str;
+          default = config.networking.hostName;
+          description = ''
+            The name of the configuration to evaluate and deploy. 
+            This value is used by comin to evaluate the flake output
+            nixosConfigurations."<hostname>" or darwinConfigurations."<hostname>".
+            Defaults to networking.hostName - you MUST set either this option
+            or networking.hostName in your configuration.
+          '';
+        };
+        flakeSubdirectory = mkOption {
+          type = str;
+          default = ".";
+          description = ''
+            Subdirectory in the repository, containing flake.nix.
+          '';
+        };
+        exporter = mkOption {
+          description = "Options for the Prometheus exporter.";
+          default = { };
+          type = submodule {
+            options = {
+              listen_address = mkOption {
+                type = str;
+                description = ''
+                  Address to listen on for the Prometheus exporter. Empty string will listen on all interfaces.
+                '';
+                default = "";
+              };
+              port = mkOption {
+                type = int;
+                description = ''
+                  Port to listen on for the Prometheus exporter.
+                '';
+                default = 4243;
+              };
+              openFirewall = mkOption {
+                type = types.bool;
+                default = false;
+                description = ''
+                  Open port in firewall for incoming connections to the Prometheus exporter.
+                '';
+              };
             };
           };
         };
-      };
-      remotes = mkOption {
-        description = "Ordered list of repositories to pull.";
-        type = listOf (submodule {
-          options = {
-            name = mkOption {
-              type = str;
-              description = ''
-                The name of the remote.
-              '';
-            };
-            url = mkOption {
-              type = str;
-              description = ''
-                The URL of the repository.
-              '';
-            };
-            auth = mkOption {
-              description = "Authentication options.";
-              default = {};
-              type = submodule {
-                options = {
-                  access_token_path = mkOption {
-                    type = str;
-                    default = "";
-                    description = ''
-                      The path of the auth file.
-                    '';
-                  };
-                };
+        remotes = mkOption {
+          description = "Ordered list of repositories to pull.";
+          type = listOf (submodule {
+            options = {
+              name = mkOption {
+                type = str;
+                description = ''
+                  The name of the remote.
+                '';
               };
-            };
-            timeout = mkOption {
-              type = int;
-              default = 300;
-              description = ''
-                Git fetch timeout in seconds.
-              '';
-            };
-            branches = mkOption {
-              description = "Branches to pull.";
-              default = {};
-              type = submodule {
-                options = {
-                  main = mkOption {
-                    default = {};
-                    description = "The main branch to fetch.";
-                    type = submodule {
-                      options = {
-                        name = mkOption {
-                          type = str;
-                          default = "main";
-                          description = "The name of the main branch.";
-                        };
-                      };
-                    };
-                  };
-                  testing = mkOption {
-                    default = {};
-                    description = "The testing branch to fetch.";
-                    type = submodule {
-                      options = {
-                        name = mkOption {
-                          type = str;
-                          default = "testing-${config.services.comin.hostname}";
-                          description = "The name of the testing branch.";
-                        };
-                      };
+              url = mkOption {
+                type = str;
+                description = ''
+                  The URL of the repository.
+                '';
+              };
+              auth = mkOption {
+                description = "Authentication options.";
+                default = { };
+                type = submodule {
+                  options = {
+                    access_token_path = mkOption {
+                      type = str;
+                      default = "";
+                      description = ''
+                        The path of the auth file.
+                      '';
                     };
                   };
                 };
               };
-            };
-            poller = mkOption {
-              default = {};
-              description = "The poller options.";
-              type = submodule {
-                options = {
-                  period = mkOption {
-                    type = types.int;
-                    default = 60;
-                    description = ''
-                      The poller period in seconds.
-                    '';
+              timeout = mkOption {
+                type = int;
+                default = 300;
+                description = ''
+                  Git fetch timeout in seconds.
+                '';
+              };
+              branches = mkOption {
+                description = "Branches to pull.";
+                default = { };
+                type = submodule {
+                  options = {
+                    main = mkOption {
+                      default = { };
+                      description = "The main branch to fetch.";
+                      type = submodule {
+                        options = {
+                          name = mkOption {
+                            type = str;
+                            default = "main";
+                            description = "The name of the main branch.";
+                          };
+                        };
+                      };
+                    };
+                    testing = mkOption {
+                      default = { };
+                      description = "The testing branch to fetch.";
+                      type = submodule {
+                        options = {
+                          name = mkOption {
+                            type = str;
+                            default = "testing-${config.services.comin.hostname}";
+                            description = "The name of the testing branch.";
+                          };
+                        };
+                      };
+                    };
+                  };
+                };
+              };
+              poller = mkOption {
+                default = { };
+                description = "The poller options.";
+                type = submodule {
+                  options = {
+                    period = mkOption {
+                      type = types.int;
+                      default = 60;
+                      description = ''
+                        The poller period in seconds.
+                      '';
+                    };
                   };
                 };
               };
             };
-          };
-        });
-      };
-      debug = mkOption {
-        type = types.bool;
-        default = false;
-        description = ''
-          Whether to run comin in debug mode. Be careful, secrets are shown!.
-        '';
-      };
-      machineId = mkOption {
-        type = types.nullOr types.str;
-        default = null;
-        description = ''
-          The expected machine-id of the machine configured by
-          comin. If not null, the configuration is only deployed
-          when this specified machine-id is equal to the actual
-          machine-id.
-          This is mainly useful for server migration: this allows
-          to migrate a configuration from a machine to another
-          machine (with different hardware for instance) without
-          impacting both.
-          Note it is only used by comin at evaluation.
-        '';
-      };
-      gpgPublicKeyPaths = mkOption {
-        description = "A list of GPG public key file paths. Each of this file should contains an armored GPG key.";
-        type = listOf str;
-        default = [];
-      };
-      postDeploymentCommand = mkOption {
-        description = "A path to a script executed after each
+          });
+        };
+        debug = mkOption {
+          type = types.bool;
+          default = false;
+          description = ''
+            Whether to run comin in debug mode. Be careful, secrets are shown!.
+          '';
+        };
+        machineId = mkOption {
+          type = types.nullOr types.str;
+          default = null;
+          description = ''
+            The expected machine-id of the machine configured by
+            comin. If not null, the configuration is only deployed
+            when this specified machine-id is equal to the actual
+            machine-id.
+            This is mainly useful for server migration: this allows
+            to migrate a configuration from a machine to another
+            machine (with different hardware for instance) without
+            impacting both.
+            Note it is only used by comin at evaluation.
+          '';
+        };
+        gpgPublicKeyPaths = mkOption {
+          description = "A list of GPG public key file paths. Each of this file should contains an armored GPG key.";
+          type = listOf str;
+          default = [ ];
+        };
+        postDeploymentCommand = mkOption {
+          description = "A path to a script executed after each
         deployment. comin provides to the script the following
         environment variables: `COMIN_GIT_SHA`, `COMIN_GIT_REF`,
         `COMIN_GIT_MSG`, `COMIN_HOSTNAME`, `COMIN_FLAKE_URL`,
         `COMIN_GENERATION`, `COMIN_STATUS` and `COMIN_ERROR_MSG`.";
-        type = nullOr path;
-        default = null;
-        example = lib.literalExpression ''
-          pkgs.writers.writeBash "post" "echo $COMIN_GIT_SHA";
-        '';
+          type = nullOr path;
+          default = null;
+          example = lib.literalExpression ''
+            pkgs.writers.writeBash "post" "echo $COMIN_GIT_SHA";
+          '';
+        };
+        executor = mkOption {
+          description = "Select which executor will be used for evaluating and building the NixOS system configuration. Specify settings used by that executor.";
+          type = lib.types.submodule {
+            options = {
+              type = lib.mkOption {
+                type = lib.types.enum [
+                  "nix"
+                  "garnix"
+                ];
+                default = "nix";
+                description = "Type of executor to use (nix or garnix)";
+              };
+
+              garnix = lib.mkOption {
+                type = lib.types.submodule {
+                  options = {
+                    baseUrl = lib.mkOption {
+                      type = lib.types.str;
+                      default = "";
+                      description = "Base URL for Garnix executor";
+                    };
+                    cacheUrl = lib.mkOption {
+                      type = lib.types.str;
+                      default = "";
+                      description = "Cache URL for Garnix executor";
+                    };
+                    retry_interval = lib.mkOption {
+                      type = lib.types.int;
+                      default = 0;
+                      description = "Retry interval for Garnix executor";
+                    };
+                    cache_size = lib.mkOption {
+                      type = lib.types.int;
+                      default = 0;
+                      description = "Cache size for Garnix executor";
+                    };
+                  };
+                };
+                default = { };
+                description = "Configuration for Garnix executor";
+              };
+            };
+          };
+          default = { };
+        };
       };
     };
-  };
 }
