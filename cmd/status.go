86fe1e274c470c83e9b082404a758f3421773520
Antoine Eiche <lewo@abesis.fr>
2025-03-11 20:11:00 +0100

    Add status --oneline

--- cmd/status.go
+++ cmd/status.go
@@ -10,10 +10,13 @@ import (
 	"github.com/dustin/go-humanize"
 
 	"github.com/nlewo/comin/internal/builder"
+	"github.com/nlewo/comin/internal/deployer"
 	"github.com/nlewo/comin/internal/manager"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 )
+
+var statusOneline bool
 
 func getStatus() (status manager.State, err error) {
 	url := "http://localhost:4242/api/status"
@@ -42,6 +45,62 @@ func getStatus() (status manager.State, err error) {
 	return
 }
 
+func longStatus(status manager.State) {
+	fmt.Printf("Status of the machine %s\n", status.Builder.Hostname)
+	if status.NeedToReboot {
+		fmt.Printf("  Need to reboot: yes\n")
+	}
+	fmt.Printf("  Fetcher\n")
+	if status.Fetcher.RepositoryStatus.SelectedCommitShouldBeSigned {
+		if status.Fetcher.RepositoryStatus.SelectedCommitSigned {
+			fmt.Printf("    Commit %s signed by %s\n", status.Fetcher.RepositoryStatus.SelectedCommitId, status.Fetcher.RepositoryStatus.SelectedCommitSignedBy)
+		} else {
+			fmt.Printf("    Commit %s is not signed while it should be\n", status.Fetcher.RepositoryStatus.SelectedCommitId)
+		}
+	}
+	for _, r := range status.Fetcher.RepositoryStatus.Remotes {
+		fmt.Printf("    Remote %s %s fetched %s\n",
+			r.Name, r.Url, humanize.Time(r.FetchedAt),
+		)
+	}
+	fmt.Printf("  Builder\n")
+	if status.Builder.Generation != nil {
+		builder.GenerationShow(*status.Builder.Generation)
+	} else {
+		fmt.Printf("    No build available\n")
+	}
+	status.Deployer.Show("    ")
+
+}
+
+func onelineStatus(status manager.State) {
+	if status.Builder.IsEvaluating {
+		fmt.Printf("evaluating %s/%s (%s)", status.Builder.Generation.SelectedRemoteName, status.Builder.Generation.SelectedBranchName,
+			humanize.Time(status.Builder.Generation.EvalStartedAt))
+	} else if status.Builder.IsBuilding {
+		fmt.Printf("building %s/%s (%s)", status.Builder.Generation.SelectedRemoteName, status.Builder.Generation.SelectedBranchName,
+			humanize.Time(status.Builder.Generation.BuildStartedAt))
+	} else if status.Builder.Generation.EvalStatus != builder.EvalFailed {
+		fmt.Printf("⛌ %s/%s (%s)", status.Builder.Generation.SelectedRemoteName, status.Builder.Generation.SelectedBranchName,
+			humanize.Time(status.Builder.Generation.EvalEndedAt))
+	} else if status.Builder.Generation.BuildStatus == builder.BuildFailed {
+		fmt.Printf("⛌ %s/%s (%s)", status.Builder.Generation.SelectedRemoteName, status.Builder.Generation.SelectedBranchName,
+			humanize.Time(status.Builder.Generation.BuildEndedAt))
+	} else if status.Deployer.Deployment != nil {
+		switch status.Deployer.Deployment.Status {
+		case deployer.Running:
+			fmt.Printf("deploying %s/%s (%s)", status.Deployer.Deployment.Generation.SelectedRemoteName, status.Deployer.Deployment.Generation.SelectedBranchName,
+				humanize.Time(status.Deployer.Deployment.EndedAt))
+		case deployer.Failed:
+			fmt.Printf("⛌ %s/%s (%s)", status.Deployer.Deployment.Generation.SelectedRemoteName, status.Deployer.Deployment.Generation.SelectedBranchName,
+				humanize.Time(status.Deployer.Deployment.EndedAt))
+		case deployer.Done:
+			fmt.Printf("✓ %s/%s (%s)", status.Deployer.Deployment.Generation.SelectedRemoteName, status.Deployer.Deployment.Generation.SelectedBranchName,
+				humanize.Time(status.Deployer.Deployment.EndedAt))
+		}
+	}
+}
+
 var statusCmd = &cobra.Command{
 	Use:   "status",
 	Short: "Get the status of the local machine",
@@ -51,35 +110,15 @@ var statusCmd = &cobra.Command{
 		if err != nil {
 			logrus.Fatal(err)
 		}
-		fmt.Printf("Status of the machine %s\n", status.Builder.Hostname)
-		needToReboot := "no"
-		if status.NeedToReboot {
-			needToReboot = "yes"
-		}
-		fmt.Printf("  Need to reboot: %s\n", needToReboot)
-		fmt.Printf("  Fetcher\n")
-		if status.Fetcher.RepositoryStatus.SelectedCommitShouldBeSigned {
-			if status.Fetcher.RepositoryStatus.SelectedCommitSigned {
-				fmt.Printf("    Commit %s signed by %s\n", status.Fetcher.RepositoryStatus.SelectedCommitId, status.Fetcher.RepositoryStatus.SelectedCommitSignedBy)
-			} else {
-				fmt.Printf("    Commit %s is not signed while it should be\n", status.Fetcher.RepositoryStatus.SelectedCommitId)
-			}
-		}
-		for _, r := range status.Fetcher.RepositoryStatus.Remotes {
-			fmt.Printf("    Remote %s %s fetched %s\n",
-				r.Name, r.Url, humanize.Time(r.FetchedAt),
-			)
-		}
-		fmt.Printf("  Builder\n")
-		if status.Builder.Generation != nil {
-			builder.GenerationShow(*status.Builder.Generation)
+		if statusOneline {
+			onelineStatus(status)
 		} else {
-			fmt.Printf("    No build available\n")
+			longStatus(status)
 		}
-		status.Deployer.Show("    ")
 	},
 }
 
 func init() {
+	statusCmd.PersistentFlags().BoolVarP(&statusOneline, "oneline", "", false, "oneline")
 	rootCmd.AddCommand(statusCmd)
 }
