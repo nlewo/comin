6453de99cc1149bf9e6f4173446bbff1b42e56ee
Torsten Curdt <tcurdt@vafer.org>
2025-05-14 19:34:30 +0200

    pass env vars, return error

--- internal/deployer/post_deployment_command.go
+++ internal/deployer/post_deployment_command.go
@@ -4,18 +4,46 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"strings"
 )
 
-func envSha() string {
-	return "SHA"
+func envGitSha(d *Deployment) string {
+	return d.Generation.SelectedCommitId
 }
 
-func envRef() string {
-	return "REF"
+func envGitRef(d *Deployment) string {
+	return fmt.Sprintf("%s/%s", d.Generation.SelectedRemoteName, d.Generation.SelectedBranchName)
 }
 
-func envState() string {
-	return "STATE"
+func envGitMessage(d *Deployment) string {
+	return strings.Trim(d.Generation.SelectedCommitMsg, "\n")
+}
+
+func envCominGeneration(d *Deployment) string {
+	return d.Generation.UUID
+}
+
+func envCominHostname(d *Deployment) string {
+	return d.Generation.Hostname
+}
+
+func envCominState(d *Deployment) string {
+	return StatusToString(d.Status)
+}
+
+func envCominErrorMessage(d *Deployment) string {
+	return d.ErrorMsg
+}
+
+// func envCominRestart(d *Deployment) string {
+// 	if d.RestartComin {
+// 		return "true"
+// 	}
+// 	return "false"
+// }
+
+func envCominFlakeUrl(d *Deployment) string {
+	return d.Generation.FlakeUrl
 }
 
 func RunPostDeploymentCommand(command string, d *Deployment) error {
@@ -23,14 +51,23 @@ func RunPostDeploymentCommand(command string, d *Deployment) error {
 	cmd := exec.Command(command)
 
 	cmd.Env = append(os.Environ(),
-		"GIT_SHA="+envSha(),
-		"GIT_REF="+envRef(),
-		"COMIN_STATE="+envState(),
+		"GIT_SHA="+envGitSha(d),
+		"GIT_REF="+envGitRef(d),
+		"GIT_MSG="+envGitMessage(d),
+		"COMIN_HOSTNAME="+envCominHostname(d),
+		"COMIN_FLAKE_URL="+envCominFlakeUrl(d),
+		"COMIN_GENERATION="+envCominGeneration(d),
+		"COMIN_STATE="+envCominState(d),
+		// "COMIN_RESTART="+envCominRestart(d),
+		"COMIN_ERROR_MSG="+envCominErrorMessage(d),
+		// d.Generation.EvalErrStr,
+		// d.Generation.BuildErrStr
 	)
 
 	output, err := cmd.CombinedOutput()
 	if err != nil {
 		fmt.Printf("Error: %s\n", err)
+		return err
 	}
 
 	fmt.Println(string(output))
