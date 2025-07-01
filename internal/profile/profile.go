6c5def5f771cad97f5620afb62cf70f6eb82ecc4
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2025-07-01 15:33:39 +0000

    chore: remove both getSystemProfilesDir and getCominProfileDir

--- internal/profile/profile.go
+++ internal/profile/profile.go
@@ -9,14 +9,6 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-func getSystemProfilesDir() string {
-	return "/nix/var/nix/profiles/system-profiles"
-}
-
-func getCominProfileDir() string {
-	return getSystemProfilesDir() + "/comin"
-}
-
 // setSystemProfile creates a link into the directory
 // /nix/var/nix/profiles/system-profiles/comin to the built system
 // store path. This is used by the switch-to-configuration script to
@@ -25,13 +17,12 @@ func getCominProfileDir() string {
 // See https://github.com/nixos/nixpkgs/blob/df98ab81f908bed57c443a58ec5230f7f7de9bd3/pkgs/os-specific/linux/nixos-rebuild/nixos-rebuild.sh#L711
 // and https://github.com/nixos/nixpkgs/blob/df98ab81f908bed57c443a58ec5230f7f7de9bd3/nixos/modules/system/boot/loader/systemd-boot/systemd-boot-builder.py#L247
 func SetSystemProfile(operation string, outPath string, dryRun bool) (profilePath string, err error) {
-	systemProfilesDir := getSystemProfilesDir()
-	cominProfileDir := getCominProfileDir()
+	cominProfileDir := "/nix/var/nix/profiles/system-profiles/comin"
 
 	if operation == "switch" || operation == "boot" {
-		err := os.MkdirAll(systemProfilesDir, os.ModeDir)
+		err := os.MkdirAll("/nix/var/nix/profiles/system-profiles", os.ModeDir)
 		if err != nil && !os.IsExist(err) {
-			return profilePath, fmt.Errorf("nix: failed to create the profile directory: %s", systemProfilesDir)
+			return profilePath, fmt.Errorf("nix: failed to create the profile directory: %s", "/nix/var/nix/profiles/system-profiles")
 		}
 		cmdStr := fmt.Sprintf("nix-env --profile %s --set %s", cominProfileDir, outPath)
 		logrus.Infof("nix: running '%s'", cmdStr)
@@ -50,7 +41,7 @@ func SetSystemProfile(operation string, outPath string, dryRun bool) (profilePat
 			if err != nil {
 				return profilePath, fmt.Errorf("nix: failed to os.Readlink(%s)", cominProfileDir)
 			}
-			profilePath = path.Join(systemProfilesDir, dst)
+			profilePath = path.Join("/nix/var/nix/profiles/system-profiles", dst)
 			logrus.Infof("nix: the profile %s has been created", profilePath)
 		}
 	}
