9db70bf70607d6435778ad96dc4a3f6b1e648f37
Kyle Petryszak <6314611+ProjectInitiative@users.noreply.github.com>
2025-10-24 23:55:23 -0500

    feat(deploy): Add liveliness check
    This introduces a liveliness check that can be run after a deployment to
    ensure the new generation is healthy. If the check fails, the deployment
    is automatically rolled back to the previous generation.
    
    The liveliness check is configured via the `liveliness_check_command`
    option.
    
    In addition, this commit also includes:
    
    - Updates to Go dependencies.
    - A version bump to 0.8.1.
    - The addition of `.envrc` and `.gitignore` files.

---
+++ internal/deployer/liveliness_check.go
@@ -0,0 +1,28 @@
+package deployer
+
+import (
+	"bytes"
+	"os"
+	"os/exec"
+	"text/template"
+
+	sprig "github.com/Masterminds/sprig/v3"
+	"github.com/nlewo/comin/internal/protobuf"
+)
+
+func runLivelinessCheckCommand(command string, deployment *protobuf.Deployment) (string, error) {
+	t, err := template.New("liveliness-check-command").Funcs(sprig.TxtFuncMap()).Parse(command)
+	if err != nil {
+		return "", err
+	}
+	var tpl bytes.Buffer
+	if err := t.Execute(&tpl, deployment); err != nil {
+		return "", err
+	}
+
+	cmd := exec.Command("sh", "-c", tpl.String())
+	cmd.Env = os.Environ()
+	output, err := cmd.CombinedOutput()
+
+	return string(output), err
+}
