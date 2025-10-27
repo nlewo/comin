f37133466e1d8af7ba7150e915c378e3ed09b6c1
Kyle Petryszak <6314611+ProjectInitiative@users.noreply.github.com>
2025-10-26 23:01:23 -0500

    fix: Update vendorHash for new dependency
    The `vendorHash` in `nix/package.nix` was updated to reflect the
    addition of a new dependency.
    
    The `vendor/` directory was also added to `.gitignore` to avoid
    committing it to the repository.

--- nix/package.nix
+++ nix/package.nix
@@ -36,7 +36,7 @@ buildGoModule rec {
       ../main.go
     ];
   };
-  vendorHash = "sha256-EZf9fH/4wu5oSmj7gPjUVxmQj0jggn5siVqXG4MJt04=";
+  vendorHash = "sha256-XX2hf6ahJgtpWn6XGW6g9/TJ8y3ALPV6lalbTWxK7/w=";
   ldflags = [
     "-X github.com/nlewo/comin/cmd.version=${version}"
   ];
