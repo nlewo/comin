d298d91f7af3c16d7b9e2f6c5f809274700627db
Antoine Eiche <lewo@abesis.fr>
2025-12-15 14:41:11 +0100

    Regenerate the documentation

--- nix/module-options-doc.nix
+++ nix/module-options-doc.nix
@@ -8,6 +8,11 @@ pkgs: rec {
             _module.args = pkgs.lib.mkOption {
               internal = true;
             };
+            assertions = pkgs.lib.mkOption {
+              type = pkgs.lib.types.anything;
+              description = "";
+            };
+
             networking.hostName = pkgs.lib.mkOption {
               type = pkgs.lib.types.str;
               internal = true;
