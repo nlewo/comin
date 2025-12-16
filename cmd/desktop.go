b4e1621af42b42469ca3ee97794d32cc8072c128
Antoine Eiche <lewo@abesis.fr>
2025-12-16 10:02:16 +0100

    Add comin desktop command

---
+++ cmd/desktop.go
@@ -0,0 +1,97 @@
+package cmd
+
+import (
+	"log"
+
+	"github.com/gen2brain/beeep"
+	"github.com/nlewo/comin/internal/client"
+	"github.com/nlewo/comin/internal/protobuf"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+)
+
+var title string
+
+var desktopCmd = &cobra.Command{
+	Use:   "desktop",
+	Short: "Send desktop notifications ",
+	Args:  cobra.MinimumNArgs(0),
+	Run: func(cmd *cobra.Command, args []string) {
+		if debug {
+			logrus.SetLevel(logrus.DebugLevel)
+		}
+		opts := client.ClientOpts{
+			UnixSocketPath: unixSocketPath,
+		}
+		err := beeep.Notify(title, "comin desktop notifications started", []byte{})
+		if err != nil {
+			panic(err)
+		}
+
+		client, err := client.New(opts)
+		if err != nil {
+			logrus.Fatal(err)
+		}
+		err = client.Events(func(event *protobuf.Event) error {
+			var message string
+			logrus.Debugf("received event: %s", event)
+			switch v := event.Type.(type) {
+			default:
+				logrus.Errorf("unexpected type %T", v)
+			case *protobuf.Event_Suspend_:
+				message = "agent is suspended"
+			case *protobuf.Event_Resume_:
+				message = "agent is resumed"
+			case *protobuf.Event_EvalStartedType:
+				message = "evaluation started"
+			case *protobuf.Event_EvalFinishedType:
+				dpl := event.Type.(*protobuf.Event_EvalFinishedType).EvalFinishedType.Generation
+				switch dpl.EvalStatus {
+				case "failed":
+					message = "evaluation failed"
+				case "evaluated":
+				default:
+					logrus.Errorf("unexpected evaluation status: %s", dpl.EvalStatus)
+				}
+			case *protobuf.Event_BuildStartedType:
+				message = "build started"
+			case *protobuf.Event_BuildFinishedType:
+				dpl := event.Type.(*protobuf.Event_BuildFinishedType).BuildFinishedType.Generation
+				switch dpl.BuildStatus {
+				case "failed":
+					message = "build failed"
+				case "built":
+				default:
+					logrus.Errorf("unexpected deployment status: %s", dpl.BuildStatus)
+				}
+			case *protobuf.Event_DeploymentStartedType:
+				message = "deployment started"
+			case *protobuf.Event_DeploymentFinishedType:
+				dpl := event.Type.(*protobuf.Event_DeploymentFinishedType).DeploymentFinishedType.Deployment
+				switch dpl.Status {
+				case "done":
+					message = "deployment finished"
+				case "failed":
+					message = "deployment finished"
+				default:
+					logrus.Errorf("unexpected deployment status: %s", dpl.Status)
+				}
+			}
+			if message != "" {
+				err := beeep.Notify(title, message, []byte{})
+				if err != nil {
+					logrus.Errorf("failed to send the notification: %s", err)
+				}
+			}
+			return nil
+		})
+		log.Fatalf("failed to consume to the event stream: %s", err)
+	},
+}
+
+func init() {
+	desktopCmd.PersistentFlags().BoolVarP(&debug, "debug", "", false, "debug")
+	desktopCmd.PersistentFlags().StringVarP(&title, "title", "", "comin", "the notification title")
+	desktopCmd.PersistentFlags().StringVarP(&unixSocketPath, "unix-socket-path", "", "/var/lib/comin/grpc.sock", "the GRPC Unix socket path")
+	rootCmd.AddCommand(desktopCmd)
+}
--- go.mod
+++ go.mod
@@ -18,6 +18,7 @@ require (
 
 require (
 	dario.cat/mergo v1.0.0 // indirect
+	git.sr.ht/~jackmordaunt/go-toast v1.1.2 // indirect
 	github.com/Microsoft/go-winio v0.6.1 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
@@ -25,30 +26,39 @@ require (
 	github.com/cyphar/filepath-securejoin v0.2.4 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/emirpasic/gods v1.18.1 // indirect
+	github.com/esiqveland/notify v0.13.3 // indirect
+	github.com/gen2brain/beeep v0.11.2 // indirect
 	github.com/go-git/gcfg v1.5.1-0.20230307220236-3a3c6141e376 // indirect
 	github.com/go-git/go-billy/v5 v5.5.0 // indirect
+	github.com/go-ole/go-ole v1.3.0 // indirect
+	github.com/godbus/dbus/v5 v5.2.0 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.4 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/jackmordaunt/icns/v3 v3.0.1 // indirect
 	github.com/jbenet/go-context v0.0.0-20150711004518-d14ea06fba99 // indirect
 	github.com/jonboulle/clockwork v0.4.0 // indirect
 	github.com/kevinburke/ssh_config v1.2.0 // indirect
+	github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646 // indirect
 	github.com/pjbgf/sha1cd v0.3.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/prometheus/client_model v0.5.0 // indirect
 	github.com/prometheus/common v0.48.0 // indirect
 	github.com/prometheus/procfs v0.12.0 // indirect
 	github.com/robfig/cron/v3 v3.0.1 // indirect
+	github.com/sergeymakinen/go-bmp v1.0.0 // indirect
+	github.com/sergeymakinen/go-ico v1.0.0 // indirect
 	github.com/sergi/go-diff v1.3.1 // indirect
 	github.com/skeema/knownhosts v1.2.1 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/tadvi/systray v0.0.0-20190226123456-11a2b8fa57af // indirect
 	github.com/xanzy/ssh-agent v0.3.3 // indirect
 	golang.org/x/crypto v0.39.0 // indirect
 	golang.org/x/exp v0.0.0-20240613232115-7f521ea00fb8 // indirect
 	golang.org/x/mod v0.25.0 // indirect
 	golang.org/x/net v0.41.0 // indirect
 	golang.org/x/sync v0.15.0 // indirect
-	golang.org/x/sys v0.33.0 // indirect
+	golang.org/x/sys v0.39.0 // indirect
 	golang.org/x/text v0.26.0 // indirect
 	golang.org/x/tools v0.33.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20250707201910-8d1bb00bc6a7 // indirect
